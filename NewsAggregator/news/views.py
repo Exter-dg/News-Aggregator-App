021263bdc136a05d97d554b274c0552d70217937
Exter-dg <prkandmap23@rocketmail.com>
2020-04-09 23:05:35 +0530

    Add frontend

--- NewsAggregator/news/views.py
+++ NewsAggregator/news/views.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup
 import itertools
 #from wordcloud import Wordcloud
 import matplotlib.pyplot as plt
-#cover 
+#cover
 
 #coverlist=['Headlines','Breaking News','Stories','Politics','Entertainment','Sports','World','Health']
-#word = WordCloud(max_font_size = 40).generate(coverlist) 
-#plt.figure() 
-#plt.imshow(word, interpolation ="bilinear") 
-#plt.axis("off") 
+#word = WordCloud(max_font_size = 40).generate(coverlist)
+#plt.figure()
+#plt.imshow(word, interpolation ="bilinear")
+#plt.axis("off")
 #fig1=plt.gcf()
 #plt.show()
 #fig1.savefig('cover.png',dpi=100)
@@ -38,19 +38,16 @@ for img in image:
         toi_image.append(imglink['data-src'])
 
 toi_title=toi_title[0:-13]
-toi=zip(toi_title,toi_link,toi_image)
-
-
 
 # news 18
-url="https://www.news18.com/politics/"
-r_n18 = requests.get(url)
+url1="https://www.news18.com/politics/"
+r_n18 = requests.get(url1)
 n18_news = r_n18.content
 soup_n18 = BeautifulSoup(n18_news,'html5lib')
 
 data = soup_n18.find_all('div',class_="blog-list-blog")
 
-n18_title = [] 
+n18_title = []
 n18_link = []
 n18_image = []
 
@@ -62,8 +59,33 @@ for news in data:
         for imglink in p.find_all('img'):
             n18_image.append(imglink['data-src'])
 
-n18=zip(n18_title,n18_link, n18_image)
-        
+
+toi=zip(toi_title,toi_link,toi_image,n18_title,n18_link, n18_image)
+
+
+
+# news 18
+"""url1="https://www.news18.com/politics/"
+r_n18 = requests.get(url1)
+n18_news = r_n18.content
+soup_n18 = BeautifulSoup(n18_news,'html5lib')
+
+data = soup_n18.find_all('div',class_="blog-list-blog")
+
+n18_title = []
+n18_link = []
+n18_image = []
+
+for news in data:
+    n18_title.append(news.text.strip())
+    for p in news.find_all('figure'):
+        for textlink in p.find_all('a', href=True):
+            n18_link.append(textlink['href'])
+        for imglink in p.find_all('img'):
+            n18_image.append(imglink['data-src'])
+
+n18=zip(n18_title,n18_link, n18_image)"""
+
 
 def index(req):
-    return render(req,'index.html', {'toi':toi,'n18':n18})
+    return render(req,'fiction-free/index.html', {'toi':toi})
