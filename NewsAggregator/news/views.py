b0a658a873434ab75d2b0ad19b3905b898752a2a
Exter-dg <prkandmap23@rocketmail.com>
2020-04-11 12:29:16 +0530

    Fixup

--- NewsAggregator/news/views.py
+++ NewsAggregator/news/views.py
@@ -2,23 +2,6 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 import itertools
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-import pandas as pd
-
-coverlst=['Headlines','Breaking News','Stories','Politics','Entertainment','Sports','World','Health','Video','Audio','tag','online','http','popularity','content','info','people','group','company','communication','ads','business','information']
-coverlist=""
-for i in coverlst:
-	coverlist= coverlist+i+' '
-
-word = WordCloud(max_font_size = 40).generate(coverlist)
-plt.figure()
-plt.imshow(word, interpolation ="bilinear")
-plt.axis("off")
-fig1=plt.gcf()
-plt.tight_layout(pad = 0)
-plt.show()
-fig1.savefig('cover.png',dpi=100) # save in static/images/slider/cover.png---------------------------------------------------------------------TODO
 
 
 # times of india
